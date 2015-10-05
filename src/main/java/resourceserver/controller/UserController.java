c283432b9186cbaf8945fca6a7ce2b70cff884a2
Izzy <xinjin214@gmail.com>
2015-10-05 22:48:13 +0100

    test

--- src/main/java/resourceserver/controller/UserController.java
+++ src/main/java/resourceserver/controller/UserController.java
@@ -1,5 +1,8 @@
 package resourceserver.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.web.bind.annotation.*;
 import resourceserver.data.pojo.User;
 
@@ -12,16 +15,19 @@ import java.util.concurrent.Callable;
 public class UserController {
 
     static final String USER_URL_PATH = "/user";
+    @Autowired
+    private AuthorizationServerTokenServices tokenServices;
 
     @RequestMapping(value = USER_URL_PATH + "/test", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Callable<User> test () {
-        return () -> {
-            User testUser = new User();
-            testUser.setAge(100);
-            testUser.setEmail("test@test.com");
-            return testUser;
-        };
+    public User test (OAuth2Authentication authentication) {
+
+        //String email = tokenServices.getAccessToken(authentication).getAdditionalInformation().get("user_name").toString();
+
+        User testUser = new User();
+        testUser.setAge(100);
+        testUser.setEmail(authentication.getPrincipal().toString());
+        return testUser;
     }
 
 }
